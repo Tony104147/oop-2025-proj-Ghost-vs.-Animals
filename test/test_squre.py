a81ec0b7340391db259a7a4c668d5069401fad3d
Tony104147 <tony104147@gmail.com>
2025-03-20 03:35:07 +0800

    A game for testing squre module

--- test/test_squre.py
+++ test/test_squre.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import add_path
-from lib.square import Square
+from square import Square
+from timer import game_timer
 import pygame
 import random
 
@@ -12,7 +13,7 @@ N = int(input("N: "))
 pygame.init()
 
 # 設定視窗
-width, height = 540, 540
+width, height = 540, 580
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("test")
 # 設定背景
@@ -22,48 +23,47 @@ screen.fill(backgroud_color)
 buttons = []
 button_size = width//N
 button_size_nomergin = button_size-4
-for i in range(10):
-    for j in range(10):
+for i in range(N):
+    for j in range(N):
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         buttons.append(Square(i*button_size+2,
-                              j*button_size+2,
+                              j*button_size+42,
                               button_size_nomergin,
                               button_size_nomergin,
                               color))
 
-# 更新畫面
 for obj in buttons:
     obj.draw(screen)
 pygame.display.update()
 
-count = N**2
-running = True
-while running:
+endtime_font = pygame.font.SysFont("simhei", 36)
+gamestate = 1
+while gamestate > 0:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+            gamestate = 0
+        elif 1 <= gamestate <= 2 and event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             for obj in buttons:
                 if obj.x < pos[0] < obj.x+obj.width and obj.y < pos[1] < obj.y+obj.height:
-                    if obj.color is not backgroud_color:
-                        count -= 1
-                        obj.color = backgroud_color
-                    obj.draw(screen)
-                    pygame.display.update()
+                    if gamestate == 1:
+                        gamestate = 2
+                        timer = game_timer()
+                    buttons.remove(obj)
+                    if len(buttons) == 0:
+                        gamestate = 3
                     break
-    if count == 0:
-        running = False
+        elif gamestate == 3 and event.type == pygame.KEYDOWN:
+            gamestate = 0
 
-print("end")
-endtime = pygame.time.get_ticks()
-endtime_font = pygame.font.SysFont("simhei", 36)
-text = endtime_font.render(str(float(endtime)/1000) + 's', True, (0, 0, 0))
-screen.blit(text, (0, 0))
-pygame.display.update()
+    # 更新畫面
+    if gamestate == 2:
+        screen.fill(backgroud_color)
+        for obj in buttons:
+            obj.draw(screen)
+        text = endtime_font.render(str(float(timer.end())/1000) + 's', False, (0, 0, 0))
+        screen.blit(text, (0, 0))
+        pygame.display.update()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-            pygame.quit()
+# 結束
 pygame.quit()
