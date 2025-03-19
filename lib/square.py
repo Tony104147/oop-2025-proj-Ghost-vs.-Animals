263399b830823cd7bc899c5b6d032b0572a7487e
Tony104147 <tony104147@gmail.com>
2025-03-20 03:33:11 +0800

    Add Square_hollow

--- lib/square.py
+++ lib/square.py
@@ -9,8 +9,20 @@ class Square:
         self.color = color
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
     def move(self, x, y):
         self.x = x
         self.y = y
+
+
+class Square_hollow(Square):
+    def __init__(self, x, y, width, height, color, thickness):
+        super().__init__(x, y, width, height, color)
+        self.thickness = thickness
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), self.thickness)
+
+    def move(self, x, y):
+        super().move(x, y)
