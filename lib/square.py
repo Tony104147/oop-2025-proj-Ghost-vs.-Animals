b445b13a01ddd218c26aeb0a585e035775c8b38c
Tony104147 <tony104147@gmail.com>
2025-03-20 01:47:26 +0800

    Create library folder \"lib/\" and a new module \"square\".

---
+++ lib/square.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Square:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
