534b4a4ead1f6ea8dcfad7a28bfd9b56eea037d1
Tony104147 <tony104147@gmail.com>
2025-03-20 01:52:20 +0800

    Create test folder and do some test for "pygame".

---
+++ test/emptywindow.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("test")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
