e6c82f089cef023ec3e5e692d1df78ad7a7d58ce
Tony104147 <tony104147@gmail.com>
2025-06-15 22:58:55 +0800

    Update event module

--- lib/event.py
+++ lib/event.py
@@ -1,57 +1,18 @@
 # -*- coding utf-8 -*-
 
-from enum import Enum, unique
-from collections import namedtuple
-
 import pygame
 
-Event = namedtuple("Event", ["ID", "type", "func", "kwargs"])
+def new(name: str):
+    if name not in TYPES.keys():
+        TYPES[name] = pygame.event.custom_type()
+        return True
+    return False
 
-@unique
-class PygameEvent(Enum):
-    '''
-    Standard events from "Pygame"
-    @
-    MOUSEMOTION : pygame.MOUSEMOTION
-    MOUSEDOWN   : pygame.MOUSEBUTTONDOWN
-    MOUSEUP     : pygame.MOUSEBUTTONUP
-    KEYDOWN     : pygame.KEYDOWN
-    KEYUP       : pygame.KEYUP
-    '''
-    MOUSEMOTION = pygame.MOUSEMOTION
-    MOUSEDOWN   = pygame.MOUSEBUTTONDOWN
-    MOUSEUP     = pygame.MOUSEBUTTONUP
-    KEYDOWN     = pygame.KEYDOWN
-    KEYUP       = pygame.KEYUP
+def post(name: str, kwargs: dict = {}):
+    if name in TYPES.keys():
+        pygame.event.post(pygame.event.Event(TYPES[name], kwargs))
+        return True
+    return False
 
-def react(event: Event):
-    return event.func(event.kwargs)
 
-# class Reactions:
-#     ''''''
-#     def __init__(self):
-#         self.events: dict[Enum, set] = dict()
-
-#     ''''''
-#     def add(self, event: Event, action):
-#         if event["type"] not in self.events.keys():
-#             self.events[event["type"]] = set()
-#         self.events[event["type"]].add(action)
-
-#     ''''''
-#     def remove(self, event: Event, action):
-#         if event["type"] in self.events.keys():
-#             self.events[event["type"]].remove(action)
-#             # To ignore situation that action not in set, use the following:
-#             #
-#             # self.events[event["type"]].discard(action)
-
-#     ''''''
-#     def clear(self, event: Event):
-#         if event["type"] in self.events.keys():
-#             self.events[event["type"]].clear()
-
-#     ''''''
-#     def react(self, event: Event, **kwargs):
-#         for action in self.events.get(event["type"], []):
-#             action(**kwargs)
+TYPES: dict[str, int] = dict()
